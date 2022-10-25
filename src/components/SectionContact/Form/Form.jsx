4514fcd27c151152f166a62e7366b6fc717b0f77
Mariia Korol <marie.korol1991@gmail.com>
2022-10-25 19:13:27 +0300

    form

--- src/components/SectionContact/Form/Form.jsx
+++ src/components/SectionContact/Form/Form.jsx
@@ -5,7 +5,7 @@ export default function Form() {
   return (
     <div>
       <form
-        action="https://korolmariia.netlify.app/contact"
+        // action="/pages/Contact"
         name="contact v2"
         method="post"
         data-netlify="true"
@@ -13,11 +13,9 @@ export default function Form() {
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact v2" />
-
         <div hidden>
           <input name="bot-field" />
         </div>
-
         <div className="form-box">
           <input
             type="text"
@@ -27,7 +25,6 @@ export default function Form() {
             required
           />
         </div>
-
         <div className="form-box">
           <input
             id="email"
@@ -38,93 +35,28 @@ export default function Form() {
             required
           />
         </div>
-
         <div className="form-box">
           <input
             type="text"
-            name="email"
+            name="subject"
             className="form-box-input"
             placeholder="Subject *"
             required
           />
         </div>
-
         <div className="form-box">
           <textarea
-            name="comments"
+            name="message"
             rows="6"
             className="contact-form-field"
             placeholder="Write message"
             required
           ></textarea>
         </div>
-
         <button type="submit" className="form-box-btn">
           Send Massage
         </button>
       </form>
     </div>
-    // <form
-    //   onSubmit={handleSubmit()}
-    //   name="contact"
-    //   method="POST"
-    //   data-netlify="true"
-    //   action="https://korolmariia.netlify.app/contact"
-    //   data-netlify-honeypot="bot-field"
-    // >
-    //   <input type="hidden" name="bot-field" />
-    //   <input type="hidden" name="form-name" value="contact" />
-    //   <div className="form-box">
-    //     <input
-    //       {...register('name', {
-    //         required: true,
-    //         pattern: /^[A-Za-z]+$/i,
-    //       })}
-    //       className="form-box-input"
-    //       type="text"
-    //       placeholder="Name *"
-    //       name="name"
-    //     />
-    //     {errors.name?.type === 'required' && (
-    //       <p className="error name">This field is required</p>
-    //     )}
-    //     {errors?.name?.type === 'pattern' && (
-    //       <p className="error name">Alphabetical characters only</p>
-    //     )}
-    //   </div>
-    //   <div className="form-box">
-    //     <input
-    //       {...register('email', { required: true })}
-    //       className="form-box-input"
-    //       type="email"
-    //       placeholder="Email*"
-    //       name="email"
-    //     />
-    //     {errors.email?.type === 'required' && (
-    //       <p className="error email">This field is required</p>
-    //     )}
-    //   </div>
-    //   <div className="form-box">
-    //     <input
-    //       {...register('subject', { required: true })}
-    //       className="form-box-input"
-    //       type="text"
-    //       placeholder="Subject*"
-    //       name="subject"
-    //     />
-    //     {errors.subject?.type === 'required' && (
-    //       <p className="error subject">This field is required</p>
-    //     )}
-    //   </div>
-    //   <div className="form-box">
-    //     <textarea
-    //       name="message"
-    //       rows="6"
-    //       className="contact-form-field"
-    //       placeholder="Write message"
-    //     ></textarea>
-    //   </div>
-    //   <input type="submit" className="form-box-btn" value="Send Massage" />
-    // </form>
   );
 }
