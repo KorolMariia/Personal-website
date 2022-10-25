0ff9d90417cd449d2e698e492d3f09c197989242
Mariia Korol <marie.korol1991@gmail.com>
2022-10-25 19:48:45 +0300

    form

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function PageSkills() {
+export default function Success() {
   const navigate = useNavigate();
 
   function navigateBackToMain() {
