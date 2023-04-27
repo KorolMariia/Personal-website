eebd79197fda778ee8122bc465270f1d36c799aa
Mariia Korol <marie.korol1991@gmail.com>
2023-04-27 14:15:47 +0300

    fixed section project

--- src/components/SectionProjects/SectionProjects.jsx
+++ src/components/SectionProjects/SectionProjects.jsx
@@ -6,7 +6,6 @@ export default function SectionProjects() {
   return (
     <div className="section projects">
       <div>
-        {' '}
         <h2 className="projects-title">
           My <span className="color">Projects</span>
         </h2>
@@ -14,10 +13,10 @@ export default function SectionProjects() {
       <div>
         <ul className="projects-list">
           <ListProjects
-            href={'https://searchmovies-opal.vercel.app/'}
-            src={'item-2.png'}
-            alt={'Movies Search'}
-            title={'Movies Search'}
+            href={'https://nutri-chef.vercel.app/'}
+            src={'nutrichef.png'}
+            alt={'NutriChef'}
+            title={'NutriChef'}
             content={
               <>
                 <p className="projects-list-content">
@@ -27,10 +26,8 @@ export default function SectionProjects() {
                 <ul className="projects-list-content">
                   <li>ReactJS: functional components and hooks</li>
                   <li>ReduxJS/toolkit</li>
-                  <li>React-routers</li>
-                  <li>React-hook-form for validation form</li>
-                  <li>SASS</li>
-                  <li>TMDB API</li>
+                  <li>React-router-dom</li>
+                  <li>Edamam API</li>
                   <li>Axios</li>
                   <li>Redux-thunk</li>
                   <li>Redux-logger</li>
@@ -39,11 +36,38 @@ export default function SectionProjects() {
                 </ul>
               </>
             }
-            hrefGit={'https://github.com/KorolMariia/Search-movies-ReactJS'}
+            hrefGit={'https://github.com/KorolMariia/NutriChef'}
+          />
+          <ListProjects
+            href={'https://git-battle-redux-toolkit.vercel.app/'}
+            src={'gitbattle.png'}
+            alt={'Git-battle'}
+            title={'Git-battle'}
+            content={
+              <>
+                <p className="projects-list-content">
+                  This project was bootstrapped with Create React App.
+                </p>
+                <span className="projects-list-content">I used:</span>
+                <ul className="projects-list-content">
+                  <li>ReactJS: functional components and hooks</li>
+                  <li>ReduxJS/toolkit</li>
+                  <li>React-router-dom</li>
+                  <li>TypeScript</li>
+                  <li>GITHUB API</li>
+                  <li>Axios</li>
+                  <li>Redux-thunk</li>
+                  <li>Redux-logger</li>
+                  <li>Material-UI</li>
+                  <li>Versel for Demo</li>
+                </ul>
+              </>
+            }
+            hrefGit={'https://github.com/KorolMariia/Git-battle-ReduxToolkit-TypeScript'}
           />
           <ListProjects
             href={'https://hlegals.netlify.app/'}
-            src={'item-3.png'}
+            src={'hlegals.png'}
             alt={'HLegals'}
             title={'HLegals'}
             content={
@@ -62,8 +86,36 @@ export default function SectionProjects() {
             hrefGit={'https://github.com/KorolMariia/HLegals-home'}
           />
           <ListProjects
+            href={'https://searchmovies-opal.vercel.app/'}
+            src={'moviesearch.png'}
+            alt={'Movies Search'}
+            title={'Movies Search'}
+            content={
+              <>
+                <p className="projects-list-content">
+                  This project was bootstrapped with Create React App.
+                </p>
+                <span className="projects-list-content">I used:</span>
+                <ul className="projects-list-content">
+                  <li>ReactJS: functional components and hooks</li>
+                  <li>ReduxJS/toolkit</li>
+                  <li>React-router-dom</li>
+                  <li>React-hook-form for validation form</li>
+                  <li>SASS</li>
+                  <li>TMDB API</li>
+                  <li>Axios</li>
+                  <li>Redux-thunk</li>
+                  <li>Redux-logger</li>
+                  <li>Material-UI</li>
+                  <li>Versel for Demo</li>
+                </ul>
+              </>
+            }
+            hrefGit={'https://github.com/KorolMariia/Search-movies-ReactJS'}
+          />
+          <ListProjects
             href={'https://you-tube-api-six.vercel.app/'}
-            src={'item-4.png'}
+            src={'youtubeapi.png'}
             alt={'Youtube-API'}
             title={'Youtube API'}
             content={
@@ -84,69 +136,28 @@ export default function SectionProjects() {
             hrefGit={'https://github.com/KorolMariia/YouTube-API'}
           />
           <ListProjects
-            href={'https://korolmariia.netlify.app/'}
-            src={'item-8.png'}
-            alt={'Website-Portfolio'}
-            title={'Website-Portfolio'}
+            href={'https://burger-factory.netlify.app/'}
+            src={'burgerfactory.png'}
+            alt={'Burger Factory'}
+            title={'Burger Factory'}
             content={
               <>
-                <p className="projects-list-content">
-                  This project was bootstrapped with Create React App.
-                </p>
                 <span className="projects-list-content">I used:</span>
                 <ul className="projects-list-content">
-                  <li>ReactJS: functional components</li>
-                  <li>React-routers</li>
-                  <li>CSS-animation</li>
+                  <li>HTML</li>
+                  <li>CSS</li>
                   <li>SASS</li>
+                  <li>JS</li>
                   <li>Adaptive, responsive, @media-queries</li>
-                  <li>Material-UI</li>
+                  <li>Netlify for deploy</li>
                 </ul>
               </>
             }
-            hrefGit={'https://github.com/KorolMariia/WebSite-portfolio'}
+            hrefGit={'https://github.com/KorolMariia/Burger-Factory'}
           />
-          <ListProjects
-            href={'https://korolmariia.github.io/Form-registrarion/'}
-            src={'item-6.png'}
-            alt={'Form-registrarion'}
-            title={'Multi-step Form-registrarion'}
-            content={
-              <>
-                <p className="projects-list-content">
-                  ReactJS with saving the data of the previous step and loading
-                  the avatar.
-                </p>
-                <span className="projects-list-content">I used:</span>
-                <ul className="projects-list-content">
-                  <li>ReactJS</li>
-                  <li>Context and Reduser</li>
-                  <li>SASS</li>
-                  <li>Material-UI</li>
-                  <li>Form validation with Yup</li>
-                  <li>React-toastify</li>
-                </ul>
-              </>
-            }
-            hrefGit={'https://github.com/KorolMariia/Form-registrarion'}
-          />
-          <ListProjects
-            href={'https://jonyray.com/'}
-            src={'item-1.png'}
-            alt={'ART Gallery Jony Ray'}
-            title={'ART Gallery Jony Ray'}
-            content={
-              <>
-                <p className="projects-list-content">
-                  This project was bootstrapped with WordPress.
-                </p>
-              </>
-            }
-          />
-
           <ListProjects
             href={'https://korolmariia.github.io/Rick-Morty-API/'}
-            src={'item-5.png'}
+            src={'RickMortyAPI.png'}
             alt={'Rick-Morty-API'}
             title={'Rick&Morty API'}
             content={
@@ -164,21 +175,17 @@ export default function SectionProjects() {
             hrefGit={'https://github.com/KorolMariia/Rick-Morty-API'}
           />
           <ListProjects
-            href={'https://korolmariia.github.io/Some-Product-Site/'}
-            src={'item-7.png'}
-            alt={'Some Product Site'}
-            title={'Some Product Site'}
+            href={'https://jonyray.com/'}
+            src={'artgallery.png'}
+            alt={'ART Gallery Jony Ray'}
+            title={'ART Gallery Jony Ray'}
             content={
               <>
-                <span className="projects-list-content">I used:</span>
-                <ul className="projects-list-content">
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>BEM methodology</li>
-                </ul>
+                <p className="projects-list-content">
+                  This project was bootstrapped with WordPress.
+                </p>
               </>
             }
-            hrefGit={'https://github.com/KorolMariia/Some-Product-Site'}
           />
         </ul>
       </div>
