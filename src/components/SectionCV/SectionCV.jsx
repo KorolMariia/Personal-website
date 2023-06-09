63b9b4bdadab4fab9501455bdadb48910731e381
Mariia Korol <marie.korol1991@gmail.com>
2023-06-09 15:20:31 +0300

    new cv

--- src/components/SectionCV/SectionCV.jsx
+++ src/components/SectionCV/SectionCV.jsx
@@ -7,7 +7,7 @@ export default function SectionCV() {
       <img className="section-cv-img" src="/images/CV.webp" alt="CV" />
       <div className="section-cv-description">
         <a
-          href="/CV/CV Trainee-Junior Front End Korol_Mariia.pdf"
+          href="/CV/CV Trainee Front End Korol_Mariia.pdf"
           download
           className="btn-download"
         >
